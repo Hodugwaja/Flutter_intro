31f5409864ac989b1c8c99b737075ea6679110bc
HoduGwaja <honeybutter9250@gmail.com>
2021-04-24 11:21:24 +0900

    [commit | 2021.04.24 finished learning UI]

--- scattfold/lib/main.dart
+++ scattfold/lib/main.dart
@@ -4,28 +4,61 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title : "Flutter Scattfold",
-      theme : ThemeData(
+      title: "Flutter Scattfold",
+      theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home : ScaffoldSample(),
+      home: ScaffoldSample(),
     );
   }
 }
 
-class ScaffoldSample extends StatefulWidget{
+class ScaffoldSample extends StatefulWidget {
   @override
   _ScaffoldSampleState createState() => _ScaffoldSampleState();
 }
 
-class ScaffoldSampleState extends State<ScaffoldSample>{
+class _ScaffoldSampleState extends State<ScaffoldSample> {
   @override
-  Widget build(BuildContext context){
-    return Container();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Scaffold 알아보기')),
+      body: Center(
+        child: TextField(),
+      ),
+      drawer: Drawer(
+        child: Center(
+          child: Text("Slide Contents"),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child : Icon(Icons.add),
+        backgroundColor: Colors.green,
+        onPressed: (){
+          print("PRESSED");
+        }
+      ),
+      resizeToAvoidBottomInset: false,
+      drawerEdgeDragWidth: 100,
+      drawerScrimColor: Colors.black.withOpacity(0.8),
+      drawerEnableOpenDragGesture: true,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        onTap: (index) {
+          print(index);
+        },
+        backgroundColor: Colors.white,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.mail), label: "messages"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
   }
 }
