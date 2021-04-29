b0c520281e9509c632ec1005b05998787fb584d4
HoduGwaja <honeybutter9250@gmail.com>
2021-04-30 08:51:44 +0900

    [commit | 2021.04.30 | commit for temporary]

--- carrot_market_turtorial/lib/page/home.dart
+++ carrot_market_turtorial/lib/page/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
+import '../repository/contents_repository.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,83 +9,25 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<Map<String, String>> Data = [];
+  String currentLocation;
+  ContentsRepository contentsRepository;
+  final Map<String, String> locationTypeToString = {
+    "youseong": "대전광역시 유성구",
+    "danwon": "안산시 단원구",
+    "seocho": "서울특별시 서초구",
+  };
 
   @override
   void initState() {
     super.initState();
-    Data = [
-      {
-        "image": 'assets/images/youseong-1.jpg',
-        "title": '제목',
-        "location": "대전광역시 유성구 장동 23-9",
-        "price": '4딸라',
-        'likes': '1121'
-      },
-      {
-        "image": 'assets/images/youseong-2.jpg',
-        "title": '제목',
-        "location": "대전광역시 유성구 장동 23-9",
-        "price": '4딸라',
-        'likes': '1972'
-      },
-      {
-        "image": 'assets/images/youseong-3.jpg',
-        "title": '제목',
-        "location": "대전광역시 유성구 장동 23-9",
-        "price": '4딸라',
-        'likes': '1972'
-      },
-      {
-        "image": 'assets/images/youseong-4.jpg',
-        "title": '미안하다 이거 보여주려고 어그로 끌었다. 솔직히 저런 거 정말로 멋지지 않나? 가슴이 장웅해진다',
-        "location": "대전광역시 유성구 장동 23-9",
-        "price": '4딸라',
-        'likes': '1972'
-      },
-      {
-        "image": 'assets/images/youseong-5.jpg',
-        "title": '상하이 나들목 단돈 4백만 딸라! 놓치지 마세요',
-        "location": "대전광역시 유성구 장동 23-9",
-        "price": '4백만 딸라',
-        'likes': '1972'
-      },
-      {
-        "image": 'assets/images/youseong-6.jpg',
-        "title": '카나미 천재',
-        "location": "대전광역시 유성구 장동 23-9",
-        "price": '4딸라',
-        'likes': '1972'
-      },
-      {
-        "image": 'assets/images/youseong-7.jpg',
-        "title": '나공익 볼짤',
-        "location": "대전광역시 유성구 장동 23-9",
-        "price": '4딸라',
-        'likes': '1972'
-      },
-      {
-        "image": 'assets/images/youseong-8.jpg',
-        "title": '규카츠 먹으려고 산건데 규카츠를 사자마자 다 먹어서 팝니다 ㅜㅜ',
-        "location": "대전광역시 유성구 장동 23-9",
-        "price": '4딸라',
-        'likes': '1972'
-      },
-      {
-        "image": 'assets/images/youseong-9.jpg',
-        "title": '우주원은 귀엽다',
-        "location": "대전광역시 유성구 장동 23-9",
-        "price": '4딸라',
-        'likes': '1972'
-      },
-      {
-        "image": 'assets/images/youseong-10.jpg',
-        "title": '세젤귀 비상식량 맛젤',
-        "location": "대전광역시 유성구 장동 23-9",
-        "price": '4딸라',
-        'likes': '1972'
-      },
-    ];
+    currentLocation = "youseong";
+    contentsRepository = ContentsRepository();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    contentsRepository = ContentsRepository();
   }
 
   final oCcy = new NumberFormat('#,###', "ko_kr");
@@ -100,12 +43,17 @@ class _HomeState extends State<Home> {
           print("click");
         },
         child: PopupMenuButton<String>(
-          offset : Offset(0, 25),
+          offset: Offset(0, 25),
           shape: ShapeBorder.lerp(
             RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
             RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
             1,
           ),
+          onSelected: (String where) {
+            setState(() {
+              currentLocation = where;
+            });
+          },
           itemBuilder: (BuildContext context) {
             return [
               PopupMenuItem(value: 'youseong', child: Text("대전광역시 유성구")),
@@ -115,7 +63,7 @@ class _HomeState extends State<Home> {
           },
           child: Row(
             children: [
-              Text("대전광역시 유성구"),
+              Text(locationTypeToString[currentLocation]),
               Icon(Icons.arrow_drop_down),
             ],
           ),
@@ -139,90 +87,101 @@ class _HomeState extends State<Home> {
     );
   }
 
+  _loadContents() {
+    return contentsRepository.loadContentsFromLocation(currentLocation);
+  }
+
   Widget _bodyWidget() {
-    return ListView.separated(
-      itemCount: Data.length,
-      itemBuilder: (BuildContext _context, int index) {
-        return Container(
-          padding: const EdgeInsets.symmetric(vertical: 10),
-          margin: const EdgeInsets.symmetric(horizontal: 10),
-          child: Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(5)),
-                child: Image.asset(
-                  Data[index]["image"],
-                  width: 100,
-                  height: 100,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  height: 100,
-                  padding: EdgeInsets.only(left: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        Data[index]["title"],
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          fontFamily: 'NotoSansCJKkr',
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        Data[index]["location"],
-                        style: TextStyle(
-                          fontFamily: 'NotoSansCJKkr',
-                          fontSize: 12,
-                          color: Colors.black.withOpacity(0.3),
-                        ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      SizedBox(height: 5),
-                      Text(
-                        Data[index]["price"],
-                        style: TextStyle(
-                          fontFamily: 'NotoSansCJKkr',
-                          fontSize: 15,
-                          fontWeight: FontWeight.w700,
-                        ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      SizedBox(height: 5),
-                      Expanded(
-                        child: Container(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              SvgPicture.asset(
-                                'assets/svg/heart_off.svg',
-                                width: 13,
-                                height: 13,
-                              ),
-                              SizedBox(width: 5),
-                              Text(Data[index]["likes"]),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
+    return FutureBuilder(
+      future: _loadContents(),
+      builder: (BuildContext context, dynamic snapshot) {
+       List<Map<String, String>> Data = snapshot.Data;
+        return ListView.separated(
+          itemCount: Data.length,
+          itemBuilder: (BuildContext _context, int index) {
+            return Container(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                    child: Image.asset(
+                      Data[index]["image"],
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
+                  Expanded(
+                    child: Container(
+                      height: 100,
+                      padding: EdgeInsets.only(left: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            Data[index]["title"],
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontFamily: 'NotoSansCJKkr',
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            Data[index]["location"],
+                            style: TextStyle(
+                              fontFamily: 'NotoSansCJKkr',
+                              fontSize: 12,
+                              color: Colors.black.withOpacity(0.3),
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            Data[index]["price"],
+                            style: TextStyle(
+                              fontFamily: 'NotoSansCJKkr',
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 5),
+                          Expanded(
+                            child: Container(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  SvgPicture.asset(
+                                    'assets/svg/heart_off.svg',
+                                    width: 13,
+                                    height: 13,
+                                  ),
+                                  SizedBox(width: 5),
+                                  Text(Data[index]["likes"]),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            );
+          },
+          separatorBuilder: (BuildContext _context, int index) {
+            return Container(height: 1, color: Color(0xff999999));
+          },
         );
       },
-      separatorBuilder: (BuildContext _context, int index) {
-        return Container(height: 1, color: Color(0xff999999));
-      },
     );
+
   }
 
   @override
---
+++ carrot_market_turtorial/lib/repository/contents_repository.dart
@@ -0,0 +1,157 @@
+class ContentsRepository {
+  // ignore: non_constant_identifier_names
+  Map<String, dynamic> Data = {
+    "youseong": [
+      {
+        "image": 'assets/images/youseong-1.jpg',
+        "title": '제목',
+        "location": "대전광역시 유성구 장동 23-9",
+        "price": '4딸라',
+        'likes': '1121'
+      },
+      {
+        "image": 'assets/images/youseong-2.jpg',
+        "title": '제목',
+        "location": "대전광역시 유성구 장동 23-9",
+        "price": '4딸라',
+        'likes': '1972'
+      },
+      {
+        "image": 'assets/images/youseong-3.jpg',
+        "title": '제목',
+        "location": "대전광역시 유성구 장동 23-9",
+        "price": '4딸라',
+        'likes': '1972'
+      },
+      {
+        "image": 'assets/images/youseong-4.jpg',
+        "title": '미안하다 이거 보여주려고 어그로 끌었다. 솔직히 저런 거 정말로 멋지지 않나? 가슴이 장웅해진다',
+        "location": "대전광역시 유성구 장동 23-9",
+        "price": '4딸라',
+        'likes': '1972'
+      },
+      {
+        "image": 'assets/images/youseong-5.jpg',
+        "title": '상하이 나들목 단돈 4백만 딸라! 놓치지 마세요',
+        "location": "대전광역시 유성구 장동 23-9",
+        "price": '4백만 딸라',
+        'likes': '1972'
+      },
+      {
+        "image": 'assets/images/youseong-6.jpg',
+        "title": '카나미 천재',
+        "location": "대전광역시 유성구 장동 23-9",
+        "price": '4딸라',
+        'likes': '1972'
+      },
+      {
+        "image": 'assets/images/youseong-7.jpg',
+        "title": '나공익 볼짤',
+        "location": "대전광역시 유성구 장동 23-9",
+        "price": '4딸라',
+        'likes': '1972'
+      },
+      {
+        "image": 'assets/images/youseong-8.jpg',
+        "title": '규카츠 먹으려고 산건데 규카츠를 사자마자 다 먹어서 팝니다 ㅜㅜ',
+        "location": "대전광역시 유성구 장동 23-9",
+        "price": '4딸라',
+        'likes': '1972'
+      },
+      {
+        "image": 'assets/images/youseong-9.jpg',
+        "title": '우주원은 귀엽다',
+        "location": "대전광역시 유성구 장동 23-9",
+        "price": '4딸라',
+        'likes': '1972'
+      },
+      {
+        "image": 'assets/images/youseong-10.jpg',
+        "title": '세젤귀 비상식량 맛젤',
+        "location": "대전광역시 유성구 장동 23-9",
+        "price": '4딸라',
+        'likes': '1972'
+      },
+    ],
+    "danwon": [
+      {
+        "image": "assets/images/ora-1.jpg",
+        "title": "LG 통돌이세탁기 15kg(내부)",
+        "location": "안산시 단원구 초지동 화랑로 250",
+        "price": "150000",
+        "likes": "13"
+      },
+      {
+        "image": "assets/images/ora-2.jpg",
+        "title": "3단책장 전면책장 가져가실분",
+        "location": "안산시 단원구 초지동 화랑로 250",
+        "price": "무료나눔",
+        "likes": "6"
+      },
+      {
+        "image": "assets/images/ora-3.jpg",
+        "title": "브리츠 컴퓨터용 스피커",
+        "location": "안산시 단원구 초지동 화랑로 250",
+        "price": "1000",
+        "likes": "4"
+      },
+      {
+        "image": "assets/images/ora-4.jpg",
+        "title": "안락 의자",
+        "location": "안산시 단원구 초지동 화랑로 250",
+        "price": "10000",
+        "likes": "1"
+      },
+      {
+        "image": "assets/images/ora-5.jpg",
+        "title": "어린이책 무료드림",
+        "location": "안산시 단원구 초지동 화랑로 250",
+        "price": "무료나눔",
+        "likes": "1"
+      },
+      {
+        "image": "assets/images/ora-6.jpg",
+        "title": "어린이책 무료드림",
+        "location": "안산시 단원구 초지동 화랑로 250",
+        "price": "무료나눔",
+        "likes": "0"
+      },
+      {
+        "image": "assets/images/ora-7.jpg",
+        "title": "칼세트 재제품 팝니다",
+        "location": "안산시 단원구 초지동 화랑로 250",
+        "price": "20000",
+        "likes": "5"
+      },
+      {
+        "image": "assets/images/ora-8.jpg",
+        "title": "아이팜장난감정리함팔아요",
+        "location": "안산시 단원구 초지동 화랑로 250",
+        "price": "30000",
+        "likes": "29"
+      },
+      {
+        "image": "assets/images/ora-9.jpg",
+        "title": "한색책상책장수납장세트 팝니다.",
+        "location": "안산시 단원구 초지동 화랑로 250",
+        "price": "1500000",
+        "likes": "1"
+      },
+      {
+        "image": "assets/images/ora-10.jpg",
+        "title": "순성 데일리 오가닉 카시트",
+        "location": "안산시 단원구 초지동 화랑로 250",
+        "price": "60000",
+        "likes": "8"
+      },
+    ],
+  };
+
+  // ignore: missing_return
+  Future<List<Map<String, String>>> loadContentsFromLocation(String location) async{
+    //API 통신 location값을 보내주면서
+
+    await Future.delayed(Duration(milliseconds: 1000));
+    return Data[location];
+  }
+}
